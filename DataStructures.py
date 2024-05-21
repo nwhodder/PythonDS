25d84d6b357e2b27161555c462755b5ec35f3229
Nicholas Hodder <Nicholas.Hodder@keyin.com>
2024-05-21 15:15:49 -0230

    added misc comments

--- DataStructures.py
+++ DataStructures.py
@@ -1,6 +1,9 @@
 class DataStructures:
+
+    #Initiate Class Array(List) from inputted integers
     def __init__(self, *args):
         self.list = [*args]
 
+    #Test method to get sum of all integers in list
     def addall(self):
         return sum(self.list)
