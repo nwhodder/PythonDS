0ee5fc925cac346e5b9505f6c284a33c45b0dabd
Nicholas Hodder <Nicholas.Hodder@keyin.com>
2024-05-22 08:47:06 -0230

    Various practice problems

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 import DataStructures
 
-ds = DataStructures.Queue(1, 2, 4)
+# Create function baba which uses a queue to print every combination of "a" and "b" with l length
+def print_baba(l):
+    ds = DataStructures.Queue('')
+    while len(ds.list):
+        word = ds.remove()
+        if len(word) == l:
+            print(word)
+        if len(word) < l:
+            ds.add(word + "a")
+            ds.add(word + "b")
+    
+# print_baba(2)
 
-print(ds)
+# Create function stairs which uses a queue print the amount of possible ways to climb n number of stairs if you can take 1 or 2 steps at a time
+def climb_stairs(n):
+    ds = DataStructures.Queue(0)
+    num_ways = 0
+    while len(ds.list):
+        stairs = ds.remove()
+        if stairs == n:
+            num_ways += 1
+        if stairs < n:
+            ds.add(stairs + 1)
+            ds.add(stairs + 2)
+    return num_ways
 
-print(ds.add(3))
+# print(climb_stairs(10))
 
-print(ds.remove())
+# Create function valid_p which uses a stack to determine if string s has all mathing parenthesis or not
+def valid_p(s):
+    s_list = [*s]
+    ds = DataStructures.Stack()
+    for e in s_list:
+        ds.add(e)
+    sum1 = 0
+    sum2 = 0
+    while len(ds.list):
+        char = ds.remove()
+        if char == "(":
+            sum1 += 1
+        if char == ")":
+            sum2 += 1
+    if sum1 == sum2 and sum1 >= 1:
+        return True
+    else:
+        return False
 
-print(ds)
+
+# print(valid_p("()"))
+# print(valid_p("()())"))
